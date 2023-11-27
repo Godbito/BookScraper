f85ea6c6f347240e7c5c5cbc283904aa12ac2b13
Godbito <garbuinitzky@gmail.com>
2023-11-27 09:46:48 -0400

    Save the book's name and author in a dictionary

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5
 resp = httpx.get(url, headers=headers)
 html = HTMLParser(resp.text)
 books = html.css("div.div-block-10")
-myBooks={}
+recomendedBooks = []
 for book in books:
-    myBooks = {
+    recomendedBooks.append({
         "name": book.css_first(".book-title").text(),
-        "author": book.css_first(".heading-9").text(),
-    }
+        "author": book.css_first(".heading-9").text()
+    })
+
+print(recomendedBooks)
 
 
