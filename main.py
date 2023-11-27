27d8d2dff5a98ef6c98a883ee182e6eeb0d030ac
Godbito <garbuinitzky@gmail.com>
2023-11-27 12:30:16 -0400

    Added a function to handle use case where one of the fields Doesn't contain any information

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ from selectolax.parser import HTMLParser
 
 url = 'https://www.kevinrooke.com/book-recommendations/peter-thiel'
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 OPR/104.0.0.0"}
-
 resp = httpx.get(url, headers=headers)
 html = HTMLParser(resp.text)
+def extract_text(html, sel):
+    try:
+        return html.css_first(sel).text()
+    except AttributeError:
+        return None
+
 books = html.css("div.div-block-10")
 recomendedBooks = []
 for book in books:
     recomendedBooks.append({
-        "name": book.css_first(".book-title").text(),
-        "author": book.css_first(".heading-9").text()
+        "name": extract_text(book, ".book-title"),
+        "author": extract_text(book,".heading-9")
     })
 
 print(recomendedBooks)
